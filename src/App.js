43b6ba096324452f18f18353b3d0e1b6841ba621
abhay <abhay.singh@tothenew.comgit config --global user.name abhaygit config --global user.email abhay.singh@tothenew.com>
2025-02-18 14:44:29 +0530

    add: Todo List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Counter from './Counter'
 import Parent from './Parent';
+import Todo from './TodoList';
 
 const App = () => {
   return (
       // <Counter />
-      <Parent/>
+      // <Parent/>
+      <Todo/>
   )
 }
 
---
+++ src/TodoList/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Todo = () => {
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState("");
+
+  const handleTodos = () => {
+    setTodos([...todos, todo]);
+    setTodo("");
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h2>Todo List</h2>
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button
+        onClick={handleTodos}
+        style={{
+          margin: 10,
+        }}
+      >
+        Add Todo
+      </button>
+
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
